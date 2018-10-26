5cfedcb4b507e1e96e9f208954315b616308eb71
liudongdonggoforit <liudongdong_922@163.com>
2018-10-26 17:18:48 +0800

    render_template 模板测试

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
+from flask import render_template
+
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello world!"
+	user = {'name': 'miguel'}
+	return  render_template("index.html", title = 'home', user = user)
