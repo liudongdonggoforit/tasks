c86f90b498104655c1d6bfdab8153ab406788f4c
liudongdonggoforit <liudongdong_922@163.com>
2018-10-31 18:19:16 +0800

    file upload  and  index.xml

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
+#coding:utf-8
 from app import app
 from flask import render_template,Flask,request
-import pymysql,json
+import pymysql,json,datetime
 
+#获取所有科目
 @app.route('/todo/api/v1.0/subjects', methods=['GET'])
 def getSubjects():
 	db = pymysql.connect("118.25.217.21", "root", "123456", "task")
@@ -26,14 +28,70 @@ def getSubjects():
 @app.route('/index')
 def index():
 	subjects = json.loads(getSubjects())
-	return render_template("index.html", subjects = subjects)
+	tasks = json.loads(get_tasks())
+	return render_template("index.html", subjects = subjects, tasks = tasks)
+
+#获取所有任务
+@app.route('/todo/api/v1.0/tasks')
+def get_tasks():
+	print("get_tasks")
+	db = pymysql.connect("118.25.217.21", "root", "123456", "task")
+	cursor = db.cursor()
+	sql = "select * from tb_tasks"
+	try:
+		cursor.execute(sql)
+		tasks = cursor.fetchall()
+		data = []
+		for task in tasks:
+			result = {}
+			result["t_id"] = task[0]
+			result["title"] = task[2]
+			result["des"] = task[3]
+			result["createtime"] = task[4]
+			result["finishtime"] = task[5]
+			result["content"] = task[6]
+
+			data.append(result)
+	except Exception as e:
+		print(e)
+		db.rollback()
+	db.close()
+	return json.dumps(data, default=datetime_handler)
+
+@app.route('/todo/api/v1.0/tasks_by_sid', methods=['GET'])
+def get_tasks_by_sid():
+	s_id = long(request.args.get('subject_id'))
+	print(s_id)
+	db = pymysql.connect("118.25.217.21", "root", "123456", "task")
+	cursor = db.cursor()
+	sql = "select * from tb_tasks where s_id = %d" % s_id
+	print(sql)
+	data = []
+	try:
+		cursor.execute(sql)
+		tasks = cursor.fetchall()
+		for task in tasks:
+			result = {}
+			result["t_id"] = task[0]
+			result["title"] = task[2]
+			result["des"] = task[3]
+			result["createtime"] = task[4]
+			result["finishtime"] = task[5]
+			result["content"] = task[6]
+
+			data.append(result)
+	except Exception as e:
+		db.rollback()
+	db.close()
+	return json.dumps(data,default=datetime_handler)
 
 
-@app.route('/todo/api/v1.0/add_project', methods=['POST'])
+#添加新科目
+@app.route('/todo/api/v1.0/add_subject', methods=['POST'])
 def add_project():
 	print(123456)
 	data = {}
-	name = request.form.get('project')
+	name = request.form.get('subject')
 	db = pymysql.connect("118.25.217.21","root","123456","task")
 	cursor = db.cursor()
 	sql = "insert into tb_subject(s_name) value ('%s')" % (name)
@@ -45,7 +103,30 @@ def add_project():
 	db.close()
 	return json.dumps(data)
 
+#增加任务
+@app.route('/todo/api/v1.0/add_task', methods=['POST'])
+def add_task():
+	print("add_task")
+	db = pymysql.connect("118.25.217.21","root", "123456", "task")
+	cursor = db.cursor()
+	id = long(request.form.get('project_id'))
+	title = request.form.get('title')
+	content = request.form.get('content')
+	des = request.form.get('des')
+	createtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+	sql = "insert into tb_tasks(s_id, t_title, t_content, t_des, t_createtime)" \
+	" values ('%d','%s','%s','%s','%s')" % (id, title, content, des, createtime)
+	try:
+		print(sql)
+		cursor.execute(sql)
+		db.commit()
+	except:
+		db.rollback()
+	db.close()
+	return "add_task"
+
+#测试
 @app.route('/register', methods=['POST'])
 def register():
 	print(request.headers)
@@ -53,3 +134,8 @@ def register():
 	print(request.form.getlist('name'))
 	print(request.form.get('nickname', default='little apple'))
 	return 'welcome'
+
+def datetime_handler(x):
+	if isinstance(x, datetime.datetime):
+		return x.isoformat()
+	raise TypeError("Unknown type")
